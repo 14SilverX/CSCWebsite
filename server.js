e8469c1dee9ff2f328fb70fa1f52477b4beb3a58
Alex Guerra <silverx@verizon.net>
2016-11-10 10:29:48 -0500

    add a style sheet

--- server.js
+++ server.js
@@ -1,13 +1,37 @@
-var express = require('express');
-var path = require('path');
-//Alex Guerra
-var app = express();
-var port = process.env.PORT || 3000;
+var http = require('http')
+  , fs   = require('fs')
+  , url  = require('url')
+  , port = 8080;
 
-app.use('/', function(req, res) {
-  res.sendFile(path.join(__dirname, '/index.html'));
-});
+var server = http.createServer (function (req, res) {
+  var uri = url.parse(req.url)
 
-app.listen(port, function() {
-  console.log('App is listening on port ' + port);
-});
+  switch( uri.pathname ) {
+    case '/':
+      sendFile(res, 'public/index.html')
+      break
+    case '/index.html':
+      sendFile(res, 'public/index.html')
+      break
+    case '/css/style.css':
+      sendFile(res, 'style.css', 'text/css')
+      break
+    default:
+      res.end('404 not found')
+  }
+})
+
+server.listen(process.env.PORT || port);
+console.log('listening on 8080')
+
+// subroutines
+
+function sendFile(res, filename, contentType) {
+  contentType = contentType || 'text/html';
+
+  fs.readFile(filename, function(error, content) {
+    res.writeHead(200, {'Content-type': contentType})
+    res.end(content, 'utf-8')
+  })
+
+}
