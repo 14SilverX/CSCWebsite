501c2bb07066e37f3daa21c46a499cea35283ec1
Alex Guerra <silverx@verizon.net>
2016-11-15 15:31:45 -0500

    run through the server file

--- server.js
+++ server.js
@@ -4,6 +4,12 @@ var http = require('http')
   , port = 8080;
 
 var server = http.createServer (function (req, res) {
+	players = 
+    fs.readFileSync('roster.txt', 'utf8')
+      .toString()
+      .trim()
+      .split("\n");
+      
   var uri = url.parse(req.url)
 
   switch( uri.pathname ) {
@@ -13,7 +19,55 @@ var server = http.createServer (function (req, res) {
     case '/index.html':
       sendFile(res, 'index.html')
       break
-    case '/css/style.css':
+    case '/roster.txt':
+      sendFile(res, 'roster.txt', 'text/txt')
+      break
+    case '/roster':
+    	showRoster(res)
+    	break
+    case '/editroster':
+    	var d = '';
+  	req.on('data', function(c) {
+    	d = d+c
+  	})
+  	req.on('end', function() {
+    	if(d != '') {
+      		var q = qs.parse(d)
+      		if(q.newplayer) {
+        		players.push( q.newplayer )
+        		fs.writeFileSync('roster.txt', players.join('\n'))
+        		sendIndex(res)
+      		}
+      		else if(q.findplayer){
+      			players = players.filter(function(p){
+      				return p.substring(0, q.findplayer.length) == q.findplayer
+      			})
+      			sendIndex(res)
+      		}
+      		else if(q.removeplayer){
+      			fs.writeFileSync('roster.txt', players.filter(function(p){
+      				return p != q.removeplayer
+      			}).join('\n'))
+      			players = 
+    fs.readFileSync('roster.txt', 'utf8')
+      .toString()
+      .trim()
+      .split("\n");
+        		sendIndex(res)
+      		}
+      		else{
+      			players = 
+    fs.readFileSync('roster.txt', 'utf8')
+      .toString()
+      .trim()
+      .split("\n");
+        		sendIndex(res)
+      		}
+    	}
+    	d = ''
+  	})
+    	break
+    case '/style.css':
       sendFile(res, 'style.css', 'text/css')
       break
     default:
@@ -23,6 +77,44 @@ var server = http.createServer (function (req, res) {
 
 server.listen(process.env.PORT || port);
 console.log('listening on 8080')
+
+function showRoster(res){
+	res.writeHead(200, {"Content-Type": "text/html"});
+
+  res.write("<html>");
+  res.write("<head>");
+  res.write("<link rel=\"stylesheet\" type=\"text/css\" href=\"style.css\" title=\"style-sheet\"/>");
+  res.write("</head>");
+  res.write("<body>");
+
+  res.write("<header><h1>WPI CSC Roster</h1></header>");
+  players.forEach(function(d) {
+    res.write("<p>" + d + "</p>");
+  });
+
+  res.write("<footer><form action=\"/updateroster\" method=\"post\">");
+  res.write("<div>");
+  res.write("<label for=\"newplayer\" > Add New Player </label>");
+  res.write("<input id=\"newplayer\" name=\"newplayer\" type=\"text\">");
+  res.write("<button type=\"submit\">Submit</button>");
+  res.write("</div>");
+  
+  res.write("<div>");
+  res.write("<label for=\"findplayer\" > Find Player </label>");
+  res.write("<input id=\"findplayer\" name=\"findplayer\" type=\"text\">");
+  res.write("<button type=\"search\">Search</button>");
+  res.write("</div>");
+  
+  res.write("<label for=\"removeplayer\" > Remove Player </label>");
+  res.write("<input id=\"removeplayer\" name=\"removeplayer\" type=\"text\">");
+  res.write("<button type=\"remove\">Remove</button>");
+  res.write("</form></footer>");
+
+
+  res.write("</body>");
+  res.write("</html>");
+  res.end();
+}
 
 // subroutines
 
